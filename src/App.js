6a77d5ffae96110a82888b06556b5e965a1c360f
тест <nigayolga95@gmail.com>
2024-01-28 18:53:50 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png'
+import bulldog from './assets/Bulldog.png'
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [advice, setAdvice] = useState("Simply tap the button and instantly receive a fresh idea to entertain yourself.")
+
+  //  Решила не использовать, так как хочу чтобы совет появлялся только после клика на кнопку
+  // useEffect(() => {
+  //   getAdvice()
+  // }, [])
+
+  const getAdvice = async () => {
+    const response = await fetch(`http://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    setAdvice(data.activity)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container logo-img-bgd">
+        <h1>
+          <img src={logo} width='500px' />
+        </h1>
+        <h2>
+          Your antidote to <br /> boredom
+        </h2>
+        <img src={bulldog} width='400px' />
+
+      </div>
+
+      <div className='container btn-bgd'>
+        <div className='advice'>
+          <p className='advice-style'>{advice}</p>
+        </div>
+
+        <div className='copy-container'>
+
+        </div>
+
+
+        <button onClick={getAdvice} className='btn'>
+          <p className='btn-copy-style'>GET IDEA</p>
+        </button >
+      </div>
     </div>
   );
 }
